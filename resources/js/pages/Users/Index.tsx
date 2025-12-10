d0dca01eebbe901facfa1d8bb7cfa6caf38eecca
333algarcia-source <333algarcia@gmail.com>
2025-12-10 09:33:36 -0600

    vista controlador

--- resources/js/pages/Users/Index.tsx
+++ resources/js/pages/Users/Index.tsx
@@ -1,7 +1,6 @@
 import AppLayout from '@/layouts/app-layout';
 import { BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
-import { useState } from 'react';
 
 interface User {
     id: number;
@@ -9,10 +8,10 @@ interface User {
     email: string;
 }
 
-const [data, setData] = useState({
+/*const [data, setData] = useState({
     nombre: '',
     email: '',
-});
+});*/
 
 const breadcrumb: BreadcrumbItem[] = [{ title: 'USUARIOS', href: '/users' }];
 
@@ -20,6 +19,9 @@ export default function Index() {
     return (
         <AppLayout breadcrumbs={breadcrumb}>
             <Head title="Users" />
+            <div>
+                <h1>Listado de usuarios</h1>
+            </div>
         </AppLayout>
     );
 }
